2b058a10e5ae34c5896cbf76c615ffcf82082391
asila99 <alisa.abdulova1309@gmail.com>
2024-08-17 17:41:37 +0400

    Project

--- src/HandEvaluator.java
+++ src/HandEvaluator.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class HandEvaluator {
 
@@ -19,107 +20,29 @@ public class HandEvaluator {
         return HandType.HIGH_CARD;
     }
 
-    private static boolean isStraightFlush(List<Card> cards) {
-        return isFlush(cards) && isStraight(cards);
-    }
-
-    private static boolean isFourOfAKind(List<Card> cards) {
-        return hasSameRank(cards, 4);
-    }
-
-    private static boolean isFullHouse(List<Card> cards) {
-        return hasSameRank(cards, 3) && hasSameRank(cards, 2);
-    }
-
-    private static boolean isFlush(List<Card> cards) {
-        Map<Character, List<Card>> suitMap = new HashMap<>();
-        for (Card card : cards) {
-            suitMap.putIfAbsent(card.getSuit(), new ArrayList<>());
-            suitMap.get(card.getSuit()).add(card);
-            if (suitMap.get(card.getSuit()).size() >= 5) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean isStraight(List<Card> cards) {
-        Set<Integer> uniqueRanks = new TreeSet<>();
-        for (Card card : cards) {
-            uniqueRanks.add(card.getRank());
-        }
-
-        if (uniqueRanks.size() < 5) {
-            return false;
-        }
-
-        List<Integer> sortedRanks = new ArrayList<>(uniqueRanks);
-
-        for (int i = 0; i <= sortedRanks.size() - 5; i++) {
-            if (sortedRanks.get(i + 4) - sortedRanks.get(i) == 4) {
-                return true;
-            }
-        }
-
-        if (sortedRanks.contains(12) && sortedRanks.subList(0, 4).equals(Arrays.asList(0, 1, 2, 3))) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private static boolean isThreeOfAKind(List<Card> cards) {
-        return hasSameRank(cards, 3);
-    }
-
-    private static boolean isTwoPair(List<Card> cards) {
-        int pairs = 0;
-        Map<Integer, Integer> rankMap = new HashMap<>();
-        for (Card card : cards) {
-            rankMap.put(card.getRank(), rankMap.getOrDefault(card.getRank(), 0) + 1);
-        }
-        for (int count : rankMap.values()) {
-            if (count == 2) {
-                pairs++;
-            }
-        }
-        return pairs >= 2;
-    }
-
-    private static boolean isOnePair(List<Card> cards) {
-        return hasSameRank(cards, 2);
-    }
-
-    private static boolean hasSameRank(List<Card> cards, int count) {
-        Map<Integer, Integer> rankMap = new HashMap<>();
-        for (Card card : cards) {
-            rankMap.put(card.getRank(), rankMap.getOrDefault(card.getRank(), 0) + 1);
-        }
-        return rankMap.containsValue(count);
-    }
-
     public static int compareHands(List<Card> hand1, HandType handType1, List<Card> hand2, HandType handType2) {
-        int comparison = handType1.compareTo(handType2);
-        if (comparison != 0) {
-            return comparison;
+        int typeComparison = handType1.compareTo(handType2);
+        if (typeComparison != 0) {
+            return typeComparison;
         }
 
         switch (handType1) {
             case STRAIGHT_FLUSH:
                 return compareStraightFlush(hand1, hand2);
             case FOUR_OF_A_KIND:
-                return compareByRankFrequency(hand1, hand2, 4);
+                return compareFourOfAKind(hand1, hand2);
             case FULL_HOUSE:
-                return compareByRankFrequency(hand1, hand2, 3);
+                return compareFullHouse(hand1, hand2);
             case FLUSH:
-                return compareHighestCard(hand1, hand2);
+                return compareFlush(hand1, hand2);
             case STRAIGHT:
                 return compareStraight(hand1, hand2);
             case THREE_OF_A_KIND:
-                return compareByRankFrequency(hand1, hand2, 3);
+                return compareThreeOfAKind(hand1, hand2);
             case TWO_PAIR:
+                return compareTwoPair(hand1, hand2);
             case ONE_PAIR:
-                return compareByRankFrequency(hand1, hand2, 2);
+                return compareOnePair(hand1, hand2);
             case HIGH_CARD:
                 return compareHighestCard(hand1, hand2);
             default:
@@ -127,14 +50,145 @@ public class HandEvaluator {
         }
     }
 
+    private static boolean isStraightFlush(List<Card> hand) {
+        return isFlush(hand) && isStraight(hand);
+    }
+
+    private static boolean isFourOfAKind(List<Card> hand) {
+        return getRankWithFrequency(hand, 4) != -1;
+    }
+
+    private static boolean isFullHouse(List<Card> hand) {
+        return getRankWithFrequency(hand, 3) != -1 && getRankWithFrequency(hand, 2) != -1;
+    }
+
+    private static boolean isFlush(List<Card> hand) {
+        return hand.stream().collect(Collectors.groupingBy(Card::getSuit, Collectors.counting())).size() == 1;
+    }
+
+    private static boolean isStraight(List<Card> hand) {
+        List<Integer> ranks = hand.stream().map(Card::getRank).distinct().sorted().collect(Collectors.toList());
+        if (ranks.size() < 5) return false;
+
+        for (int i = 0; i <= ranks.size() - 5; i++) {
+            if (ranks.get(i + 4) - ranks.get(i) == 4) return true;
+        }
+
+        if (ranks.contains(12) && ranks.subList(0, 4).equals(Arrays.asList(0, 1, 2, 3))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean isThreeOfAKind(List<Card> hand) {
+        return getRankWithFrequency(hand, 3) != -1;
+    }
+
+    private static boolean isTwoPair(List<Card> hand) {
+        List<Long> pairs = hand.stream()
+                .collect(Collectors.groupingBy(Card::getRank, Collectors.counting()))
+                .values().stream().filter(count -> count == 2).collect(Collectors.toList());
+        return pairs.size() >= 2;
+    }
+
+    private static boolean isOnePair(List<Card> hand) {
+        return getRankWithFrequency(hand, 2) != -1;
+    }
+
     private static int compareStraightFlush(List<Card> hand1, List<Card> hand2) {
-        return compareStraight(hand1, hand2);
+        return compareStraight(hand2, hand1);
+    }
+
+    private static int compareFourOfAKind(List<Card> hand1, List<Card> hand2) {
+        int rank1 = getRankWithFrequency(hand1, 4);
+        int rank2 = getRankWithFrequency(hand2, 4);
+        if (rank1 != rank2) {
+            return Integer.compare(rank1, rank2);
+        }
+        return compareKicker(hand2, hand1, Arrays.asList(rank2));
+    }
+
+    private static int compareFullHouse(List<Card> hand1, List<Card> hand2) {
+        int threeOfAKindRank1 = getRankWithFrequency(hand1, 3);
+        int threeOfAKindRank2 = getRankWithFrequency(hand2, 3);
+        if (threeOfAKindRank1 != threeOfAKindRank2) {
+            return Integer.compare(threeOfAKindRank1, threeOfAKindRank2);
+        }
+        int pairRank1 = getRankWithFrequency(hand1, 2);
+        int pairRank2 = getRankWithFrequency(hand2, 2);
+        return Integer.compare(pairRank2, pairRank1);
+    }
+
+    private static int compareFlush(List<Card> hand1, List<Card> hand2) {
+        return compareHighestCard(hand2, hand1);
     }
 
     private static int compareStraight(List<Card> hand1, List<Card> hand2) {
         int highCard1 = getHighestCardInStraight(hand1);
         int highCard2 = getHighestCardInStraight(hand2);
-        return Integer.compare(highCard1, highCard2);
+        return Integer.compare(highCard2, highCard1);
+    }
+
+    private static int compareThreeOfAKind(List<Card> hand1, List<Card> hand2) {
+        return compareByRankFrequency(hand2, hand1, 3);
+    }
+
+    private static int compareTwoPair(List<Card> hand1, List<Card> hand2) {
+        int highPair1 = getHighestRankByFrequency(hand1, 2);
+        int highPair2 = getHighestRankByFrequency(hand2, 2);
+        if (highPair1 != highPair2) {
+            return Integer.compare(highPair2, highPair1);
+        }
+
+        List<Card> remainingHand1 = removeRank(hand1, highPair1);
+        List<Card> remainingHand2 = removeRank(hand2, highPair2);
+
+        int lowPair1 = getHighestRankByFrequency(remainingHand1, 2);
+        int lowPair2 = getHighestRankByFrequency(remainingHand2, 2);
+        if (lowPair1 != lowPair2) {
+            return Integer.compare(lowPair2, lowPair1);
+        }
+
+        return compareHighestCard(remainingHand1, remainingHand2);
+    }
+
+    private static int compareOnePair(List<Card> hand1, List<Card> hand2) {
+        return compareByRankFrequency(hand2, hand1, 2);
+    }
+
+    private static int compareHighestCard(List<Card> hand1, List<Card> hand2) {
+        for (int i = 0; i < hand1.size(); i++) {
+            int comparison = Integer.compare(hand2.get(i).getRank(), hand1.get(i).getRank());
+            if (comparison != 0) {
+                return comparison;
+            }
+        }
+        return 0;
+    }
+
+    private static int getRankWithFrequency(List<Card> hand, int frequency) {
+        Map<Integer, Integer> rankCount = hand.stream()
+                .collect(Collectors.groupingBy(Card::getRank, Collectors.summingInt(e -> 1)));
+        return rankCount.entrySet().stream()
+                .filter(entry -> entry.getValue() == frequency)
+                .map(Map.Entry::getKey)
+                .findFirst().orElse(-1);
+    }
+
+    private static List<Card> removeRank(List<Card> hand, int rank) {
+        return hand.stream().filter(card -> card.getRank() != rank).collect(Collectors.toList());
+    }
+
+    private static int compareByRankFrequency(List<Card> hand1, List<Card> hand2, int frequency) {
+        int rank1 = getRankWithFrequency(hand1, frequency);
+        int rank2 = getRankWithFrequency(hand2, frequency);
+        if (rank1 != rank2) {
+            return Integer.compare(rank1, rank2);
+        }
+        List<Card> remainingHand1 = removeRank(hand1, rank1);
+        List<Card> remainingHand2 = removeRank(hand2, rank2);
+        return compareHighestCard(remainingHand2, remainingHand1);
     }
 
     private static int getHighestCardInStraight(List<Card> hand) {
@@ -145,9 +199,9 @@ public class HandEvaluator {
 
         List<Integer> sortedRanks = new ArrayList<>(uniqueRanks);
 
-        for (int i = sortedRanks.size() - 1; i >= 4; i--) {
-            if (sortedRanks.get(i) - sortedRanks.get(i - 4) == 4) {
-                return sortedRanks.get(i);
+        for (int i = 0; i <= sortedRanks.size() - 5; i++) {
+            if (sortedRanks.get(i + 4) - sortedRanks.get(i) == 4) {
+                return sortedRanks.get(i + 4);
             }
         }
 
@@ -158,24 +212,36 @@ public class HandEvaluator {
         return sortedRanks.get(sortedRanks.size() - 1);
     }
 
-    private static int compareHighestCard(List<Card> hand1, List<Card> hand2) {
-        for (int i = 0; i < hand1.size(); i++) {
-            int comparison = hand1.get(i).getRank() - hand2.get(i).getRank();
+    private static int getHighestRankByFrequency(List<Card> hand, int frequency) {
+        return hand.stream()
+                .collect(Collectors.groupingBy(Card::getRank, Collectors.summingInt(e -> 1)))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() == frequency)
+                .map(Map.Entry::getKey)
+                .max(Integer::compareTo)
+                .orElse(-1);
+    }
+
+    private static int compareKicker(List<Card> hand1, List<Card> hand2, List<Integer> excludedRanks) {
+        List<Integer> kickers1 = hand1.stream()
+                .filter(card -> !excludedRanks.contains(card.getRank()))
+                .map(Card::getRank)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        List<Integer> kickers2 = hand2.stream()
+                .filter(card -> !excludedRanks.contains(card.getRank()))
+                .map(Card::getRank)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < Math.min(kickers1.size(), kickers2.size()); i++) {
+            int comparison = Integer.compare(kickers1.get(i), kickers2.get(i));
             if (comparison != 0) {
                 return comparison;
             }
         }
+
         return 0;
-    }
-
-    private static int compareByRankFrequency(List<Card> hand1, List<Card> hand2, int frequency) {
-        int rank1 = getHighestRankByFrequency(hand1, frequency);
-        int rank2 = getHighestRankByFrequency(hand2, frequency);
-        return Integer.compare(rank1, rank2);
-    }
-
-    private static int getHighestRankByFrequency(List<Card> hand, int frequency) {
-        return hand.stream().filter(c -> Collections.frequency(hand, c.getRank()) == frequency)
-                .map(Card::getRank).max(Integer::compare).orElse(0);
     }
 }
--- src/PokerGame.java
+++ src/PokerGame.java
@@ -1,41 +1,65 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class PokerGame {
 
     public static void main(String[] args) {
-        Deck deck = new Deck();
+        Scanner scanner = new Scanner(System.in);
+        boolean continuePlaying = true;
 
-        List<Card> player1Hand = new ArrayList<>();
-        List<Card> player2Hand = new ArrayList<>();
-        List<Card> communityCards = new ArrayList<>();
+        while (continuePlaying) {
+            Deck deck = new Deck();
 
-        for (int i = 0; i < 2; i++) {
-            player1Hand.add(deck.dealCard());
-            player2Hand.add(deck.dealCard());
+            List<Card> player1Hand = new ArrayList<>();
+            List<Card> player2Hand = new ArrayList<>();
+            List<Card> communityCards = new ArrayList<>();
+
+            for (int i = 0; i < 2; i++) {
+                player1Hand.add(deck.dealCard());
+                player2Hand.add(deck.dealCard());
+            }
+
+            for (int i = 0; i < 5; i++) {
+                communityCards.add(deck.dealCard());
+            }
+
+            HandType player1HandType = HandEvaluator.evaluateHand(player1Hand, communityCards);
+            HandType player2HandType = HandEvaluator.evaluateHand(player2Hand, communityCards);
+
+            System.out.println("Player 1's Hand: " + player1Hand);
+            System.out.println("Player 2's Hand: " + player2Hand);
+            System.out.println("Community Cards: " + communityCards);
+
+            System.out.println("Player 1's Hand Type: " + player1HandType);
+            System.out.println("Player 2's Hand Type: " + player2HandType);
+
+            int result = HandEvaluator.compareHands(player1Hand, player1HandType, player2Hand, player2HandType);
+            if (result < 0) {
+                System.out.println("Player 1 wins!");
+            } else if (result > 0) {
+                System.out.println("Player 2 wins!");
+            } else {
+                System.out.println("It's a tie!");
+            }
+            
+            while (true) {
+                System.out.print("Do you want to play another round? (yes/no): ");
+                String input = scanner.nextLine().trim().toLowerCase();
+
+                if (input.equals("yes")) {
+                    continuePlaying = true;
+                    break;
+                } else if (input.equals("no")) {
+                    continuePlaying = false;
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter 'yes' or 'no'.");
+                }
+            }
         }
 
-        for (int i = 0; i < 5; i++) {
-            communityCards.add(deck.dealCard());
-        }
-
-        HandType player1HandType = HandEvaluator.evaluateHand(player1Hand, communityCards);
-        HandType player2HandType = HandEvaluator.evaluateHand(player2Hand, communityCards);
-
-        System.out.println("Player 1's Hand: " + player1Hand);
-        System.out.println("Player 2's Hand: " + player2Hand);
-        System.out.println("Community Cards: " + communityCards);
-
-        System.out.println("Player 1's Hand Type: " + player1HandType);
-        System.out.println("Player 2's Hand Type: " + player2HandType);
-
-        int result = HandEvaluator.compareHands(player1Hand, player1HandType, player2Hand, player2HandType);
-        if (result < 0) {
-            System.out.println("Player 1 wins!");
-        } else if (result > 0) {
-            System.out.println("Player 2 wins!");
-        } else {
-            System.out.println("It's a tie!");
-        }
+        System.out.println("Thanks for playing!");
+        scanner.close();
     }
 }
